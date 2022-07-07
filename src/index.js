21cc15db7e9894ddf7b86865ac3c77510a90c522
Marc raphael <marcraphael979@gmail.com>
2022-07-07 12:47:41 +0100

    index: fetch data from API

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import _ from 'lodash';
 import './style.css';
 import separator from './images/pattern-divider-desktop.svg';
 import dice from './images/icon-dice.svg';
+
+
+// let's get our API url
+let API_URL = 'https://api.adviceslip.com/advice';
+
+// Now we fethc it to get a Json object
+const get = async (url) => {
+  const reponse = await fetch(url);
+  return await reponse.json();
+}
+
+// Now we assign to an object the data we want
+const API = {get}
