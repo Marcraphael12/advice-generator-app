33831da1b7a6ecc574f37326fd8745b7b27b3cb2
Marc raphael <marcraphael979@gmail.com>
2022-07-07 13:29:52 +0100

    index: write get random quote function

--- src/index.js
+++ src/index.js
@@ -19,3 +19,25 @@ const API = {get}
 // import our DOM elements
 const quote = document.querySelector('.quote');
 const id = document.querySelector('.id');
+
+// Now, our function to get a random quote
+const getRandomQuote = async () => {
+  //we run the get() method in the API object wich is a JSON file
+  API.get(API_URL).then(data => {
+    // we assign the data to a variable
+    const randomQuote = data.slip.advice;
+    // we assign the id to a variable
+    const randomId = data.slip.slip_id;
+    console.log(randomId);
+    // we assign the quote to the DOM element
+    quote.innerHTML = `"${randomQuote.toString()}"`;
+    // we assign the id to the DOM element
+    id.innerHTML = `Adivce #${randomId}`;
+  })
+}
+
+const reloadButton = document.querySelector('.get-quote')
+reloadButton.addEventListener("click", ()=> getRandomQuote());
+
+// Now we call our function when the page loads
+document.body.onload = getRandomQuote();
